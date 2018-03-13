6dfe3f322d218c68b4f6c09cb15bedc5377a883f
Wells <Joe.Wells@am.jll.com>
2018-03-13 14:52:30 -0400

    added stock list capability

--- stockPull.py
+++ stockPull.py
@@ -1,21 +1,33 @@
 import requests
+import csv
+import numpy
 from configparser import ConfigParser
 
 config = ConfigParser()
 config.read('config/config.ini')
-
 apiKey = config.get('auth', 'apiKey')
+with open('data/stockList.csv', 'r') as d:
+    reader = csv.reader(d)
+    stockList = list(reader)
+    d.close()
+
+i = 0
+stockList = stockList[0]
 
 
-stockTicker = 'AAPL'
+while i < len(stockList):
+    stockTicker = stockList[i]
 
-URL = 'https://www.alphavantage.co/query?' \
-      'function=TIME_SERIES_DAILY_ADJUSTED&symbol=' + stockTicker + '&outputsize=full&apikey=' + apiKey
+    URL = 'https://www.alphavantage.co/query?' \
+          'function=TIME_SERIES_DAILY_ADJUSTED&symbol=' + stockTicker + '&outputsize=compact&apikey=' + apiKey
 
-response = requests.get(URL)
-print(response)
+    response = requests.get(URL)
+    print(response)
 
-nFile = open("data/" + stockTicker + "compact.json", "w+")
-nFile.write(response.text)
-nFile.close()
-print("Data saved to: " + str(nFile.name))
+    nFile = open("data/" + stockTicker + "compact.json", "w+")
+    nFile.write(response.text)
+    nFile.close()
+    print("Data saved to: " + str(nFile.name))
+    i = i + 1
+
+print('\nCompleted Data Collection for ' + str(i+1) + ' stocks.')
