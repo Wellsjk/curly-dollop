cf7dd459e72af896a76615f92037ff541c9b6586
wellsjk <Joe.Wells@am.jll.com>
2018-03-23 11:44:54 -0400

    Scanner loop added

--- scanner.py
+++ scanner.py
@@ -1,26 +1,41 @@
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
+import csv
 
 thresholdPercent = .02
-days2check = 10
-stockTicker = 'AAPL'
-file = 'data/' + stockTicker + 'compact.csv'
-data = pd.read_csv(file)
+days2check = 5
+
+with open('data/stockList.csv', 'r') as d:
+    reader = csv.reader(d)
+    stockList = list(reader)
+    d.close()
+
+i = 0
+p = 0
+stockList = stockList[0]
 
 
-# data.index = data['timestamp']
-stockData = data[['timestamp', 'close']]
+while p < len(stockList):
+    stockTicker = stockList[p]
+    file = 'data/' + stockTicker + 'compact.csv'
+    data = pd.read_csv(file)
 
-thresholdAmount = stockData['close'][days2check] - (stockData['close'][0]*thresholdPercent)
-print('Threshold amount: ' + str(thresholdAmount))
-i = days2check
-while i > 0:
-    if stockData['close'][i] < thresholdAmount:
-        print('!Dropped more than threshold on: ' + str(stockData['timestamp'][i]) + 'Price: ' + str(stockData['close'][i])+ '!!!!!!')
-        i = i - 1
-    else:
-        print('did not drop more than threshold, price: ' + str(stockData['close'][i])+ ' Date: ' + str(stockData['timestamp'][i]))
-        i = i - 1
+
+    # data.index = data['timestamp']
+    stockData = data[['timestamp', 'close']]
+
+    thresholdAmount = stockData['close'][days2check] - (stockData['close'][0]*thresholdPercent)
+    print('Threshold amount: ' + str(thresholdAmount) + '  For: ' + stockTicker)
+    i = days2check
+    while i > 0:
+        if stockData['close'][i] < thresholdAmount:
+            print('!Dropped more than threshold on: ' + str(stockData['timestamp'][i]) + ' Price: ' + str(stockData['close'][i])+ '!!!!!!')
+            i = i - 1
+        else:
+           # print('did not drop more than threshold, price: ' + str(stockData['close'][i])+ ' Date: ' + str(stockData['timestamp'][i]))
+            i = i - 1
+
+    p = p + 1
 
 
